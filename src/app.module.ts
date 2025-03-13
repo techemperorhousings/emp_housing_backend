7b017430e9f538f83e6539f0a2444f1847552892
Nanret123 <nanretgungshik@gmail.com>
2025-03-13 14:40:06 +0100

    refactor: refactor modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthenticationModule } from '@authentication/authentication.module';
-import { MailModule } from '@mail/mail.module';
 import { JwtAuthGuard } from '@guards/jwt.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { modules } from '@modules';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
-    JwtModule.register({
-      secret: process.env.SECRET_KEY_JWT,
-    }),
-    PrismaModule,
-    AuthenticationModule,
-    MailModule,
-  ],
+  imports: [...modules],
   controllers: [AppController],
   providers: [
     AppService,
