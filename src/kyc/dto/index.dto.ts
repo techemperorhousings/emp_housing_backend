40c1c4ed84f73ca8b2536b821818db0545cada09
Nanret123 <nanretgungshik@gmail.com>
2025-03-13 14:36:41 +0100

    feat: add kyc feature

---
+++ src/kyc/dto/index.dto.ts
@@ -0,0 +1,62 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsString, IsUrl, IsUUID } from 'class-validator';
+import { KycDocumentType, KycStatus } from '@prisma/client';
+
+export class CreateKycDto {
+  @ApiProperty({ example: 'USER_ID_123', description: 'User ID' })
+  @IsNotEmpty()
+  @IsUUID()
+  userId: string;
+
+  @ApiProperty({ example: 'PASSPORT', enum: KycDocumentType })
+  @IsEnum(KycDocumentType)
+  @IsNotEmpty()
+  documentType: KycDocumentType;
+
+  @ApiProperty({ example: 'https://example.com/doc.pdf' })
+  @IsNotEmpty()
+  @IsUrl()
+  documentUrl: string;
+}
+
+export class ApproveKycDto {
+  @ApiProperty({
+    description: 'KYC ID to approve',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  kycId: string;
+
+  @ApiProperty({
+    description: 'KYC status',
+    example: 'APPROVED',
+    enum: KycStatus,
+  })
+  @IsEnum(KycStatus)
+  status: KycStatus;
+}
+
+export class RejectKycDto {
+  @ApiProperty({
+    description: 'KYC ID to reject',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  kycId: string;
+
+  @ApiProperty({
+    description: 'KYC status',
+    example: 'REJECTED',
+    enum: KycStatus,
+  })
+  @IsEnum(KycStatus)
+  status: KycStatus;
+
+  @ApiProperty({
+    description: 'Reason for rejection',
+    example: 'Document is blurry and unreadable',
+  })
+  @IsString()
+  @IsNotEmpty()
+  reason: string;
+}
---
+++ src/kyc/kyc.service.ts
@@ -0,0 +1,102 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
+import { ApproveKycDto, CreateKycDto, RejectKycDto } from './dto/index.dto';
+import { KycStatus } from '@prisma/client';
+
+@Injectable()
+export class KycService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async submitKyc(dto: CreateKycDto) {
+    const existingKyc = await this.prisma.kYC.findUnique({
+      where: { userId: dto.userId },
+    });
+
+    if (existingKyc) {
+      throw new ConflictException('KYC already submitted');
+    }
+
+    const kyc = await this.prisma.kYC.create({ data: dto });
+    return {
+      message: 'KYC submitted successfully',
+      data: kyc,
+    };
+  }
+
+  async getKycByUser(userId: string) {
+    const kyc = await this.prisma.kYC.findUnique({ where: { userId } });
+    if (!kyc) throw new NotFoundException('KYC record not found');
+    return {
+      message: 'KYC fetched successfully',
+      data: kyc,
+    };
+  }
+
+  async getAllKycs() {
+    const kycs = await this.prisma.kYC.findMany();
+    return {
+      message: 'KYCs fetched successfully',
+      data: kycs,
+    };
+  }
+
+  async approveKyc(dto: ApproveKycDto) {
+    const kyc = await this.prisma.kYC.findUnique({
+      where: { id: dto.kycId },
+    });
+
+    if (!kyc) {
+      throw new NotFoundException('KYC record not found');
+    }
+
+    if (kyc.status !== KycStatus.PENDING) {
+      throw new BadRequestException('KYC is already processed');
+    }
+
+    return this.prisma.kYC.update({
+      where: { id: dto.kycId },
+      data: { status: KycStatus.APPROVED },
+    });
+  }
+
+  async rejectKyc(dto: RejectKycDto) {
+    const kyc = await this.prisma.kYC.findUnique({
+      where: { id: dto.kycId },
+    });
+
+    if (!kyc) {
+      throw new NotFoundException('KYC record not found');
+    }
+
+    if (kyc.status !== KycStatus.PENDING) {
+      throw new BadRequestException('KYC is already processed');
+    }
+
+    return this.prisma.kYC.update({
+      where: { id: dto.kycId },
+      data: {
+        status: KycStatus.REJECTED,
+        reason: dto.reason,
+      },
+    });
+  }
+
+  async deleteKyc(id: string) {
+    const kyc = await this.prisma.kYC.findUnique({
+      where: { id },
+    });
+
+    if (!kyc) {
+      throw new NotFoundException('KYC record not found');
+    }
+    await this.prisma.kYC.delete({ where: { id } });
+    return {
+      message: 'KYC deleted successfully',
+    };
+  }
+}
