c330c1767a4526e781c52187b12870acae179d48
Nanret123 <nanretgungshik@gmail.com>
2025-03-16 15:02:25 +0100

    feat: added pagination dto

---
+++ src/utils/pagination.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, Min, IsOptional, Max } from 'class-validator';
+
+export class PaginationQueryDto {
+  @ApiProperty({
+    description: 'Number of items to skip',
+    required: false,
+    default: 0,
+    minimum: 0,
+  })
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  @Type(() => Number)
+  skip?: number = 0;
+
+  @ApiProperty({
+    description: 'Number of items to take',
+    required: false,
+    default: 10,
+    minimum: 1,
+    maximum: 100,
+  })
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  @IsOptional()
+  @Type(() => Number)
+  take?: number = 10;
+}
