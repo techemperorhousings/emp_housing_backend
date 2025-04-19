ad8dc52639fec0c47c5bf692de35dfd0b6767944
Nanret123 <nanretgungshik@gmail.com>
2025-04-19 15:11:24 +0100

    fix: removefile upload module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 import { JwtAuthGuard } from '@guards/jwt.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { modules } from '@modules';
-import { adminModules } from '@admin/modules';
+import { PusherModule } from './pusher/pusher.module';
 
 @Module({
-  imports: [...modules, ...adminModules],
+  imports: [...modules],
   controllers: [AppController],
   providers: [
     AppService,
